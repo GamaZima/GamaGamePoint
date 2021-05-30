4f5edfdfa9d90b8e3feb19d586ab31bf2bc90ddd
GamaZima <84975854+GamaZima@users.noreply.github.com>
2021-05-30 19:35:50 -0300

    Enemy AI implemented

---
+++ GamaGame-PointClick/Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Interactables
+{
+    public override void Interact()
+    {
+        base.Interact();
+        // Attack the enemy
+    }
+}
