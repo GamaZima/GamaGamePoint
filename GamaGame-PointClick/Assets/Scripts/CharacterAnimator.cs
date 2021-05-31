51991eea5bfa82018bafc72bb6d9fcf5fc26c507
GamaZima <84975854+GamaZima@users.noreply.github.com>
2021-05-31 02:19:40 -0300

    Battle system updated

--- GamaGame-PointClick/Assets/Scripts/CharacterAnimator.cs
+++ GamaGame-PointClick/Assets/Scripts/CharacterAnimator.cs
@@ -5,10 +5,17 @@ using UnityEngine.AI;
 
 public class CharacterAnimator : MonoBehaviour
 {
+
+    public AnimationClip replaceableAttackAnim;
+
+    public AnimationClip[] defaultAttackAnimSet;
+    protected AnimationClip[] currentAttackAnimSet;
+
     const float locomotionAnimationSmoothTime = .1f;
     NavMeshAgent agent;
     protected Animator animator;
     protected CharacterCombat combat;
+    protected AnimatorOverrideController overrideController;
 
     // Start is called before the first frame update
     protected virtual void Start()
@@ -16,6 +23,13 @@ public class CharacterAnimator : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponentInChildren<Animator>();
         combat = GetComponent<CharacterCombat>();
+
+        overrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animator.runtimeAnimatorController = overrideController;
+
+        currentAttackAnimSet = defaultAttackAnimSet;
+        combat.OnAttack += OnAttack;
+
     }
 
     // Update is called once per frame
@@ -25,5 +39,12 @@ public class CharacterAnimator : MonoBehaviour
         animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
 
         animator.SetBool("inCombat", combat.InCombat);
+    }
+
+    protected virtual void OnAttack()
+    {
+        animator.SetTrigger("attack");
+        int attackIndex = Random.Range(0, currentAttackAnimSet.Length);
+        overrideController[replaceableAttackAnim.name] = currentAttackAnimSet[attackIndex];
     }
 }
